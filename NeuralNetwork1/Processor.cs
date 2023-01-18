48bdceb4df226896b8d3db91e1d674c937b493b8
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2023-01-18 19:39:09 +0300

    .

---
+++ NeuralNetwork1/Processor.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace NeuralNetwork1
+{
+    internal class Settings
+    {
+        private int _border = 20;
+        public int border
+        {
+            get
+            {
+                return _border;
+            }
+            set
+            {
+                if ((value > 0) && (value < height / 3))
+                {
+                    _border = value;
+                    if (top > 2 * _border) top = 2 * _border;
+                    if (left > 2 * _border) left = 2 * _border;
+                }
+            }
+        }
+
+        public int width = 640;
+        public int height = 640;
+
+        /// <summary>
+        /// Размер сетки для сенсоров по горизонтали
+        /// </summary>
+        public int blocksCount = 10;
+
+        /// <summary>
+        /// Желаемый размер изображения до обработки
+        /// </summary>
+        public Size orignalDesiredSize = new Size(500, 500);
+        /// <summary>
+        /// Желаемый размер изображения после обработки
+        /// </summary>
+        public Size processedDesiredSize = new Size(300, 300);
+
+        public int margin = 10;
+        public int top = 40;
+        public int left = 40;
+
+        /// <summary>
+        /// Второй этап обработки
+        /// </summary>
+        public bool processImg = false;
+
+        /// <summary>
+        /// Порог при отсечении по цвету 
+        /// </summary>
+        public byte threshold = 120;
+        public float differenceLim = 0.15f;
+
+        public void incTop() { if (top < 2 * _border) ++top; }
+        public void decTop() { if (top > 0) --top; }
+        public void incLeft() { if (left < 2 * _border) ++left; }
+        public void decLeft() { if (left > 0) --left; }
+    }
+
+    internal class MagicEye
+    {
+        /// <summary>
+        /// Обработанное изображение
+        /// </summary>
+        public Bitmap processed;
+        /// <summary>
+        /// Оригинальное изображение после обработки
+        /// </summary>
+        public Bitmap original;
+
+        /// <summary>
+        /// Класс настроек
+        /// </summary>
+        public Settings settings = new Settings();
+
+        
+        private BaseNetwork network;
+        private DatasetProcessor dataset;
+        public MagicEye(BaseNetwork network, DatasetProcessor dataset)
+        {
+            this.network = network;
+            this.dataset = dataset;
+        }
+
+        public bool ProcessImage(Bitmap bitmap)
+        {
+          
+            if (bitmap.Height > bitmap.Width)
+                throw new Exception("Bro what");
+            int side = bitmap.Height;
+
+
+          
+            Rectangle cropRect = new Rectangle(settings.border, settings.border, bitmap.Width - settings.border, bitmap.Height - settings.border);
+
+
+            original = bitmap;
+
+           
+            AForge.Imaging.Filters.Crop cropFilter = new AForge.Imaging.Filters.Crop(cropRect);
+            var uProcessed = cropFilter.Apply(AForge.Imaging.UnmanagedImage.FromManagedImage(original));
+            AForge.Imaging.Filters.Grayscale grayFilter = new AForge.Imaging.Filters.Grayscale(0.2125, 0.7154, 0.0721);
+            uProcessed = grayFilter.Apply(uProcessed);
+
+
+
+           
+            AForge.Imaging.Filters.ResizeBilinear scaleFilter = new AForge.Imaging.Filters.ResizeBilinear(settings.processedDesiredSize.Width, settings.processedDesiredSize.Height);
+            uProcessed = scaleFilter.Apply(uProcessed);
+ 
+
+
+            AForge.Imaging.Filters.BradleyLocalThresholding threshldFilter = new AForge.Imaging.Filters.BradleyLocalThresholding();
+            threshldFilter.PixelBrightnessDifferenceLimit = settings.differenceLim;
+            threshldFilter.ApplyInPlace(uProcessed);
+            
+            processed = uProcessed.ToManagedImage();
+            
+            if (settings.processImg)
+            {
+                currentType = processSample(processed);
+            }
+
+            return true;
+        }
+        
+        public LetterType currentType { get; set; }
+
+        /// <summary>
+        /// Обработка одного сэмпла
+        /// </summary>
+        /// <param name="index"></param>
+        private LetterType processSample(Bitmap bitmap)
+        {
+            Sample s = dataset.getSample(bitmap);
+            return network.Predict(s);
+        }
+
+    }
+}
---
+++ NeuralNetwork1/StudentNetwork.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeuralNetwork1
+{
+    class Neuron
+    {
+        public static Func<double, double> activationFunction;
+        public static Func<double, double> activationFunctionDerivative;
+
+        public int id;
+        public double Output;
+        public int layer;
+
+        public double error;
+        public double[] weightsToPrevLayer;
+
+        public void setInput(double input)
+        {
+            if (layer == 0)
+            {
+                Output = input;
+                return;
+            }
+
+            Output = activationFunction(input);
+        }
+
+        public Neuron(int id, int layer, int prevLayerCapacity, Random random)
+        {
+            this.id = id;
+            this.layer = layer;
+            this.error = 0;
+            if (layer == -1)
+            {
+                Output = 1;
+            }
+            if (layer < 1)
+            {
+                weightsToPrevLayer = null;
+            }
+            else
+            {
+                weightsToPrevLayer = new double [prevLayerCapacity + 1];
+                for (int i = 0; i < weightsToPrevLayer.Length; i++)
+                {
+                    weightsToPrevLayer[i] = random.NextDouble() * 2 - 1;
+                }
+            }
+        }
+    }
+
+    public class StudentNetwork : BaseNetwork
+    {
+        private const int hiddenLayersCount = 2;
+        private const double learningRate = 0.1;
+
+        private Neuron biasNeuron;
+        private List<Neuron[]> layers;
+
+        private Func<double[], double[], double> lossFunction;
+        private Func<double, double, double> lossFunctionDerivative;
+
+        public StudentNetwork(int[] structure)
+        {
+            if (structure.Length < 3)
+            {
+                throw new ArgumentException("Can not do 0 layers");
+            }
+
+            lossFunction = (output, aim) =>
+            {
+                double res = 0;
+                for (int i = 0; i < aim.Length; i++)
+                {
+                    res += Math.Pow(aim[i] - output[i], 2);
+                }
+
+                return res * 0.5;
+            };
+
+            lossFunctionDerivative = (output, aim) => aim - output;
+
+            Neuron.activationFunction = s => 1.0 / (1.0 + Math.Exp(-s));
+            Neuron.activationFunctionDerivative = s => s * (1 - s);
+
+            Random random = new Random();
+
+            biasNeuron = new Neuron(0, -1, -1, random);
+            int id = 1;
+
+            layers = new List<Neuron[]>();
+
+            for (int layer = 0; layer < structure.Length; layer++)
+            {
+                layers.Add(new Neuron[structure[layer]]);
+                for (int i = 0; i < structure[layer]; i++)
+                {
+                    if (layer == 0)
+                    {
+                        layers[layer][i] = new Neuron(id, layer, -1, random);
+                        continue;
+                    }
+
+                    layers[layer][i] = new Neuron(id, layer, structure[layer - 1], random);
+
+                    id++;
+                }
+            }
+        }
+
+
+        public void forwardPropagation(double[] input)
+        {
+            if (input.Length != layers[0].Length)
+            {
+                throw new ArgumentException("Вы мне подсунули какой-то странный входной массив.");
+            }
+            for (int i = 0; i < layers[0].Length; i++)
+            {
+                layers[0][i].setInput(input[i]);
+            }
+
+            for (int layer = 1; layer < layers.Count; layer++)
+            {
+                for (int neuron = 0; neuron < layers[layer].Length; neuron++) 
+                {
+                    double scalar = 0;
+
+                    for (int i = 0; i < layers[layer][neuron].weightsToPrevLayer.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+                            scalar += biasNeuron.Output * layers[layer][neuron].weightsToPrevLayer[0];
+                            continue;
+                        }
+                        scalar += layers[layer - 1][i - 1].Output * layers[layer][neuron].weightsToPrevLayer[i];
+                    }
+                    layers[layer][neuron].setInput(scalar);
+                }
+            }
+        }
+
+        public void backwardPropagation(Sample sample)
+        {
+            var aim = sample.outputVector;
+            for (var i = 0; i < layers.Last().Length; i++)
+            {
+                layers.Last()[i].error = lossFunctionDerivative(layers.Last()[i].Output, aim[i]);
+            }
+
+            for (int layer = layers.Count - 1; layer >= 1; layer--)
+            {
+                foreach (var neuron in layers[layer])
+                {
+                    neuron.error *= Neuron.activationFunctionDerivative(neuron.Output);
+                    for (int i = 0; i < neuron.weightsToPrevLayer.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+                            biasNeuron.error += neuron.error * neuron.weightsToPrevLayer[0];
+                            neuron.weightsToPrevLayer[0] += learningRate * neuron.error * biasNeuron.Output;
+                            continue;
+                        }
+
+                        layers[layer - 1][i - 1].error += neuron.error * neuron.weightsToPrevLayer[i];
+                        neuron.weightsToPrevLayer[i] += learningRate * neuron.error * layers[layer - 1][i - 1].Output;
+                    }
+                    neuron.error = 0;
+                }
+            }
+        }
+
+        public override int Train(Sample sample, double acceptableError, bool parallel)
+        {
+            int cnt = 0;
+            while (true)
+            {
+                cnt++;
+                forwardPropagation(sample.input);
+                if (lossFunction(layers.Last().Select(n => n.Output).ToArray(), sample.outputVector) <=
+                    acceptableError || cnt > 50)
+                {
+                    return cnt;
+                }
+
+                backwardPropagation(sample);
+            }
+        }
+
+        double TrainOnSample(Sample sample, double acceptableError)
+        {
+            double loss;
+            forwardPropagation(sample.input);
+            loss = lossFunction(layers.Last().Select(n => n.Output).ToArray(), sample.outputVector);
+            backwardPropagation(sample);
+            return loss;
+        }
+
+        public override double TrainOnDataSet(SamplesSet samplesSet, int epochsCount, double acceptableError,
+            bool parallel)
+        {
+            var start = DateTime.Now;
+            int totalSamplesCount = epochsCount * samplesSet.Count;
+            int processedSamplesCount = 0;
+            double sumError = 0;
+            double mean;
+            for (int epoch = 0; epoch < epochsCount; epoch++)
+            {
+                for (var index = 0; index < samplesSet.samples.Count; index++)
+                {
+                    var sample = samplesSet.samples[index];
+                    sumError += TrainOnSample(sample, acceptableError);
+
+                    processedSamplesCount++;
+                    if (index % 100 == 0)
+                    {
+                        OnTrainProgress(1.0 * processedSamplesCount / totalSamplesCount,
+                            sumError / (epoch * samplesSet.Count + index + 1), DateTime.Now - start);
+                    }
+                }
+
+                mean = sumError / ((epoch + 1) * samplesSet.Count + 1);
+                if (mean  <= acceptableError)
+                {
+                    OnTrainProgress(1.0,
+                        mean, DateTime.Now - start);
+                    return mean;
+                }
+            }
+            mean = sumError / (epochsCount * samplesSet.Count + 1);
+            OnTrainProgress(1.0,
+                       mean, DateTime.Now - start);
+            return sumError / (epochsCount * samplesSet.Count);
+        }
+
+        protected override double[] Compute(double[] input)
+        {
+            if (input.Length != layers[0].Length)
+            {
+                throw new ArgumentException("Too much data");
+            }
+
+            forwardPropagation(input);
+            return layers.Last().Select(n => n.Output).ToArray();
+        }
+    }
+}
